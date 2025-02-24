4a8b26d650c5edb217afd1a8119763e87bb1dc44
Akatsuki <nancyallam2@gmail.com>
2025-02-24 02:34:48 +0200

    Initial commit.

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'features/onboarding/onboarding_screen.dart';
+
+void main() async{
+  await ScreenUtil.ensureScreenSize();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+
+    return ScreenUtilInit(
+      designSize: const Size(393, 851),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_ , child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: child,
+        );
+      },
+      child: OnBoardingScreen(),
+    );
+  }
+}
+
